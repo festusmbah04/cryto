0550707dde5eac517a16d3eb7a68c45abd5a24c8
festusmbah04 <festusmbahgitup@gmail.com>
2024-04-16 06:33:10 -0700

    helo

--- server.js
+++ server.js
@@ -1,58 +1,47 @@
+require('dotenv').config(); // Load environment variables from a .env file
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const path = require('path'); // Require the path module
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Parse application/json
 app.use(bodyParser.json());
-
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Nodemailer configuration
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', // Specify your email service provider
+  service: 'Gmail',
   auth: {
-    user: 'festusmbahconnect@gmail.com', 
-    pass: 'ulchsarqvbpftgwz' 
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
-// Route handler for the root URL ("/")
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html')); 
 });
 
-
-// Route handler for 'Choose.html'
 app.get('/Choose.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Choose.html')); 
-  });
-  
+  res.sendFile(path.join(__dirname, 'Choose.html')); 
+});
 
-// Route handler for 'Start.html'
 app.get('/Start.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Start.html')); 
-  });
-   
-// Route to handle form submissions
-app.post('/submit_email', (req, res) => {
-  const emailText = req.body.textarea; // Get the textarea field from the form
+  res.sendFile(path.join(__dirname, 'Start.html')); 
+});
 
-  // Email content
+app.post('/submit_email', (req, res) => {
+  const emailText = req.body.textarea;
+
   const mailOptions = {
-    from: 'festusmbahconnect@gmail.com',
-    to: 'festusmbahconnect@gmail.com', // Your email address
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
     subject: 'New form submission',
     text: emailText 
   };
 
-  // Send email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error:', error);
@@ -64,7 +53,6 @@ app.post('/submit_email', (req, res) => {
   });
 });
 
-// Start the server.
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
